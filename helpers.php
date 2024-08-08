09c102eafdd6cedaae0c27dca465fb7a0f467aac
Em <zheni@debian-BULLSEYE-live-builder-AMD64>
2024-08-07 23:40:43 -0300

    Initial commit - setup project structure and partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,41 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+/**
+ * Load a view
+ *
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadView($name)
+{ // require basePath("views/{$name}.view.php");
+  $viewPath = basePath("views/{name}.view.php");
+
+  //Make sure path exits
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  }else {
+    echo "View '{name}' not found.";
+  }
+  }
+
+/**
+ * Load a partial
+ *
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadPartial($name)
+{
+  // require basePath("views/partials/{$name}.php");
+  $partialPath = basePath("views/partials/{$name}.php");
+
+  // Make sure path exists
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "Partial '{$name}' not found";
+  }
+  }
